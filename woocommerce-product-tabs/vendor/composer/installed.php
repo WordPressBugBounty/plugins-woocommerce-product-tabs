974d5d81f18865b83d7dc8fd00dca248d5b9628b
Sean Murphy <sean@iamseanmurphy.com>
2024-09-22 22:07:31 +0000

    2.1.5

---
+++ woocommerce-product-tabs/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '2.1.5.x-dev',
+        'version' => '2.1.5.9999999-dev',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'afd796b1ed009dbcd64a497cd1c26eb0fc0aa5ad',
+        'name' => 'barn2plugins/woocommerce-product-tabs',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'barn2plugins/woocommerce-product-tabs' => array(
+            'pretty_version' => '2.1.5.x-dev',
+            'version' => '2.1.5.9999999-dev',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'afd796b1ed009dbcd64a497cd1c26eb0fc0aa5ad',
+            'dev_requirement' => false,
+        ),
+    ),
+);
